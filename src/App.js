9cb2a8128edcc3b697db94e23ba25ca91deecb06
Alan Lokas <alan.lokas11@gmail.com>
2023-05-26 23:05:38 +0200

    Init App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(()=>{
+    const storageTodos = JSON.parse(localStorage.getItem('todos'));
+    if(storageTodos.length>0){
+      setTodos(storageTodos);
+    }   
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem('todos', JSON.stringify(todos));
+  },[todos]);
+
+  function addTodo(text){
+    setTodos([...todos,text]);
+  }
+
+  function removeTodo(index){
+    setTodos(todos.filter((todos, i) => i !== index));
+  }
+
+  function updateTodo(index,text){
+    const newTodos = [...todos];
+    newTodos[index] = text;
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODO List</h1>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        addTodo(e.target.elements.todo.value);
+        e.target.elements.todo.value='';
+      }}>
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo,index)=> (
+          <li key={index}>
+            <input type="text" value={todo} onChange={(e)=>updateTodo(index,e.target.value)} />
+            <button onClick={()=>removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
